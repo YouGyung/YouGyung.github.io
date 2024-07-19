e5f73f310aadf08e52c462f358847747b4f0b40f
yougyung <youkyeongs@naver.com>
2024-07-19 18:26:23 +0900

    feat: apply pretendard font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,7 @@ import "@fontsource/merriweather"
 import "./src/style/normalize.css"
 // custom CSS styles
 import "./src/style/style.css"
+import "./src/style/main.css"
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
